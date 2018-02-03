d233df856466d430f6db113b82aa6fcb4c8450b5
Sakshi13sharma <frnd.desires@gmail.com>
2018-02-03 14:12:29 -0500

    Update splashScreen.java

--- app/src/main/java/com/zoom/event/eventzoom/splashScreen.java
+++ app/src/main/java/com/zoom/event/eventzoom/splashScreen.java
@@ -9,5 +9,7 @@ public class splashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        
+        Button btn;
     }
 }
